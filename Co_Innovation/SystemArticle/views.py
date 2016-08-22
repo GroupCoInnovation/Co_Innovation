a26cd58115334b295dcff89e72b70d4845e70a8e
wangxg6 <359173080@qq.com>
2016-08-22 13:46:47 +0800

    upload

--- Co_Innovation/SystemArticle/views.py
+++ Co_Innovation/SystemArticle/views.py
@@ -150,28 +150,17 @@ def show_upload_file(request):
 @csrf_exempt
 def upload_file(request):
 
-    file = request.POST['file']
+    file = request.FILES.get['file']
     today = datetime.datetime.today()
     file_dir = settings.MEDIA_ROOT + '/%d/%d/%d/' % (today.year, today.month, today.day)
     if not os.path.exists(file_dir):
         os.makedirs(file_dir)
 
-    filename = file[12:]
-    file_path = file_dir + filename
+    file_path = file_dir + file.name
 
-    upload_url = settings.MEDIA_URL + '%d/%d/%d/'%(today.year, today.month, today.day) + filename
+    upload_url = settings.MEDIA_URL + '%d/%d/%d/'%(today.year, today.month, today.day) + file.name
     try:
         open(file_path, 'wb+').write(file.read())  # 上传文件
         return HttpResponse(upload_url)
     except:
         return HttpResponse(upload_url)
-
-
-
-
-
-
-
-
-
-
